e78f423df39f2847a46fe6a0d7fb401201767b79
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2024-08-07 10:39:32 -0500

    add algo

--- src/sections/ListaMiDia.tsx
+++ src/sections/ListaMiDia.tsx
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, type ChangeEvent } from "react";
 import "../css/ListaMiDia.css";
 import axios from "axios";
+import { format } from "date-fns";
 
 interface DatosApi{
-    id:number
+    id?:number
     tarea:string
-    fecha:Date
+    fecha:string
     importante:number
     terminada:number
 }
 
 function ListaMiDia() {
     const[datos, setDatos] = useState<DatosApi[]>([]);
+    const [tarea, setTarea] = useState<string>("");
+    let fechaActual = new Date(); 
+    let fechaActualF = format(fechaActual, "dd-MM-yyyy").toString;
 
     const cambiarEstado = (index:number)=>{
         if (datos[index].terminada == 0) {
@@ -21,8 +25,30 @@ function ListaMiDia() {
         }
     }
 
+    const cambioInput = (e:ChangeEvent<HTMLInputElement>)=>{
+        setTarea(e.target.value);
+    }
+
+    const enviar = ()=>{
+        let fecha2 = new Date();
+        let fechaFormat = format(fecha2,"dd-mm-yyyy");
+        const datosEnviar:DatosApi = {
+            tarea: tarea,
+            fecha: fechaFormat,
+            importante:0,
+            terminada:0
+        }
+        console.log(datosEnviar.fecha);
+        axios.post("http://localhost:8080/api/tarea/guardar", datosEnviar)
+        .then(respuesta =>{
+            console.log(respuesta)
+        })
+        alert("Tarea agregada: "+datosEnviar.tarea);
+        window.location.reload();
+    }
+
     const buscarDatos = ()=>{
-        axios.get("http://localhost:8080/api/tarea/buscar/todos")
+        axios.get(`http://localhost:8080/api/tarea/buscar/todos`)
         .then(respuesta =>{
             setDatos(respuesta.data)
         })
@@ -36,9 +62,9 @@ function ListaMiDia() {
     return(
         <div className="contenedorLista">
             <div className="contenedorLista-input">
-                <input type="text" name="tarea" placeholder="Ingrese una tarea" size={100}/>
+                <input type="text" name="tarea" placeholder="Ingrese una tarea" size={100} id="tarea" onChange={cambioInput}/>
                 <div className="contenedorLista-input-div">
-                    <button>Agregar</button>
+                    <button onClick={enviar}>Agregar</button>
                 </div>
             </div>
             <ul className="contenedorLista-lista">
